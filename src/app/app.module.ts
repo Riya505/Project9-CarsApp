db5f502b8c970e52296473a07b97180ea4088f5f
Riya505 <riyapk505@gmail.com>
2022-10-13 04:21:25 +0530

    new

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { CarComponent } from './car/car.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    CarComponent
   ],
   imports: [
     BrowserModule,
